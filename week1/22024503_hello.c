56d6be8ae5110c6529a9538da4f6d57d4e88c768
pknguyen2704 <pknguyen2704@gmail.com>
2025-02-16 12:26:12 +0700

    update week 1 assignment

---
+++ week1/22024503_hello.c
@@ -0,0 +1,64 @@
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+
+#define BUFFER_SIZE 128
+
+#define PROC_NAME "hello"
+#define MESSAGE "Hello World\n"
+
+static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct proc_ops proc_ops = {
+    .proc_read = proc_read,
+};
+
+static int proc_init(void)
+{
+
+
+        proc_create(PROC_NAME, 0, NULL, &proc_ops);
+
+        printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+
+    return 0;
+}
+
+static void proc_exit(void) {
+
+        // removes the /proc/hello entry
+        remove_proc_entry(PROC_NAME, NULL);
+
+        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+        int rv = 0;
+        char buffer[BUFFER_SIZE];
+        static int completed = 0;
+
+        if (completed) {
+                completed = 0;
+                return 0;
+        }
+
+        completed = 1;
+
+        rv = sprintf(buffer, "Hello World\n");
+
+        // copies the contents of buffer to userspace usr_buf
+        copy_to_user(usr_buf, buffer, rv);
+
+        return rv;
+}
+
+module_init( proc_init );
+module_exit( proc_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello Module");
+MODULE_AUTHOR("SGG");
