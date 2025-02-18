f64fa2629ba86d24442b880a646dc1f3f9ec9b12
pknguyen2704 <pknguyen2704@gmail.com>
2025-02-18 18:42:47 +0700

    update w1 assignment

--- week1/hello.c
+++ week1/hello.c
@@ -1,3 +1,14 @@
+/**
+ * hello.c
+ *
+ * Kernel module that communicates with /proc file system.
+ * 
+ * The makefile must be modified to compile this program.
+ * Change the line "simple.o" to "hello.o"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
 
 #include <linux/init.h>
 #include <linux/module.h>
@@ -10,23 +21,32 @@
 #define PROC_NAME "hello"
 #define MESSAGE "Hello World\n"
 
+/**
+ * Function prototypes
+ */
 static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
 
-static struct proc_ops proc_ops = {
-    .proc_read = proc_read,
+static struct file_operations proc_ops = {
+        .owner = THIS_MODULE,
+        .read = proc_read,
 };
 
+
+/* This function is called when the module is loaded. */
 static int proc_init(void)
 {
 
-
+        // creates the /proc/hello entry
+        // the following function call is a wrapper for
+        // proc_create_data() passing NULL as the last argument
         proc_create(PROC_NAME, 0, NULL, &proc_ops);
 
         printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
 
-    return 0;
+	return 0;
 }
 
+/* This function is called when the module is removed. */
 static void proc_exit(void) {
 
         // removes the /proc/hello entry
@@ -35,6 +55,21 @@ static void proc_exit(void) {
         printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
 }
 
+/**
+ * This function is called each time the /proc/hello is read.
+ * 
+ * This function is called repeatedly until it returns 0, so
+ * there must be logic that ensures it ultimately returns 0
+ * once it has collected the data that is to go into the 
+ * corresponding /proc file.
+ *
+ * params:
+ *
+ * file:
+ * buf: buffer in user space
+ * count:
+ * pos:
+ */
 static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
 {
         int rv = 0;
@@ -56,6 +91,8 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
         return rv;
 }
 
+
+/* Macros for registering module entry and exit points. */
 module_init( proc_init );
 module_exit( proc_exit );
 
---
+++ week1/modules.order
@@ -0,0 +1,1 @@
+/home/phung-khoi-nguyen/Dev/OS/osc10e/ch2/simple.o
